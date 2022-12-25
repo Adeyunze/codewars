8e9eba955962908257832eccfa0e91ad7bcf71ca
Adeyunze <adeyunze05@gmail.com>
2022-12-25 00:33:41 +0000

    Created increment_string.js

--- increment_string.js
+++ increment_string.js
@@ -1,3 +1,23 @@
 function incrementString (strng) {
+    // Get the alphabets in the string
+    let strAlpha = strng.replace(/[0-9]/g, '');
+
+    // Track the lenght of string
+    let length = strng.length - strAlpha.length;
+
+    // Get numbers in str
+    let numbers = strng.slice(strAlpha.length) || '0'
+
+    numbers = (parseInt(numbers) + 1).toString()
+
     
+    while(numbers.length < length){
+        numbers = '0' + numbers
+    }
+    
+    strAlpha = strAlpha.concat(numbers);
+    console.log(strAlpha);
+
 }
+
+incrementString('fooo009')
