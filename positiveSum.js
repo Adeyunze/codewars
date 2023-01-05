394fc900c4c518dc158e4820ec8c98a8ee4eb7d8
Adeyunze <adeyunze05@gmail.com>
2023-01-05 10:24:24 +0000

    Removed console.log

--- positiveSum.js
+++ positiveSum.js
@@ -1,8 +1,4 @@
 const positiveSum = arr => {
     let positive = arr.filter(num => num > 0)
-    // if(positive.length === 0)
     return positive.length ?  positive.reduce((a, b) => a + b) : 0
-    // return positive
 }
-
-console.log(positiveSum([-1,-2,-3,-4,-5]));
